165f7fd5bdd00d073ed3800613f03d41c4a94566
thisisyourfahi <fahiyanofficial@gmail.com>
2026-05-12 15:03:44 +0600

    feat: proxy on all-photos and profile page

---
+++ src/app/pricing/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PricingPage = () => {
+    return (
+        <div className="mt-10 space-y-8 border text-center h-[70vh] flex items-center justify-center">
+            <p className='text-3xl font-thin'>Pricing page is under construction.</p>
+        </div>
+    );
+};
+
+export default PricingPage;
