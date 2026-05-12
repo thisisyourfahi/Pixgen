63251b83964c168c8ca1e1f780e4d2ff18cb3cf3
thisisyourfahi <fahiyanofficial@gmail.com>
2026-05-12 15:22:04 +0600

    fix: proxy

--- src/proxy.js
+++ src/proxy.js
@@ -10,6 +10,7 @@ export async function proxy(request) {
   if (!session) {
     return NextResponse.redirect(new URL('/signin', request.url))
   }
+  return NextResponse.next();
 }
 
 // Alternatively, you can use a default export:
